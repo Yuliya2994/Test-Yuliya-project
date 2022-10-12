bc64331ad5edbb959e085ecfe75598fb20ada8ef
Yuliya <dubchuk.yuliya@gmail.com>
2022-10-12 13:08:27 +0300

    Initial Commit

---
+++ Test Yuliya project/Test_Yuliya_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Yuliya_projectApp.swift
+//  Test Yuliya project
+//
+//  Created by Yuliya Dubchuk on 12.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Test_Yuliya_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
